31c07cc97edbfc3c214e19d5059080ad1f25be04
kkolcz <kkolcz@power.com.pl>
2016-02-08 17:54:26 +0100

    util changes - add hibernate config, chagne structure

---
+++ util/src/test/java/com/kkolcz/hibernate/util/SessionUtilTest.java
@@ -0,0 +1,10 @@
+package com.kkolcz.hibernate.util;
+import org.hibernate.Session;
+import org.testng.annotations.Test;
+public class SessionUtilTest{
+  @Test
+  public void testSessionFactory() {
+    Session session = SessionUtil.getSession();
+    session.close();
+  }
+}
