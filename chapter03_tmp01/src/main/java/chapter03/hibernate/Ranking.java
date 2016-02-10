0741cb00e837a7e78043811c06888ea3c17b3022
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2016-02-10 23:07:00 +0100

    removing dependency loop

--- chapter03_tmp01/src/main/java/chapter03/hibernate/Ranking.java
+++ chapter03_tmp01/src/main/java/chapter03/hibernate/Ranking.java
@@ -4,15 +4,15 @@ import javax.persistence.*;
 public class Ranking{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  Long id;
   @Column
-  private Integer ranking;
+  Integer ranking;
   @ManyToOne
-  private Person object;
+  Person object;
   @ManyToOne
-  private Person subject;
+  Person subject;
   @ManyToOne
-  private Skill skill;
+  Skill skill;
   public Ranking() {}
   public void setId(Long id) { this.id=id; }
   public Long getId() { return id; }
