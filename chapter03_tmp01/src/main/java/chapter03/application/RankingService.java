d096feee41a6da415398fd9a6731a9c366735536
kkolcz <kkolcz@power.com.pl>
2016-02-15 18:20:15 +0100

    change Integer to int

--- chapter03_tmp01/src/main/java/chapter03/application/RankingService.java
+++ chapter03_tmp01/src/main/java/chapter03/application/RankingService.java
@@ -2,7 +2,7 @@ package chapter03.application;
  
 public interface RankingService {
   int getRankingFor(String subject, String skill);
-  void addRanking(String subject, String observer, String skill, int ranking);
+  void addRanking(String subject, String observer, String skill, Integer ranking);
     public void updateRanking(String subjectName,String objectName,String skillName,int rank);
     public void removeRanking(String subject, String observer, String skill); 
 }
