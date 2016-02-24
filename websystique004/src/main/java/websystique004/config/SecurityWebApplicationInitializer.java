e1fa57aefd366d8f7639bc5037d228d1aa016327
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2016-02-24 20:59:22 +0100

    secure view layer using taglibs added

---
+++ websystique004/src/main/java/websystique004/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package websystique004.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+ 
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
