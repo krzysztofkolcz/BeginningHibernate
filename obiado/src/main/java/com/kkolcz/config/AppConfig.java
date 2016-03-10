16f04ba3263a5f782ee2bd8a9053b14b70b88431
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2016-03-10 22:38:22 +0100

    obiado project added

---
+++ obiado/src/main/java/com/kkolcz/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.kkolcz.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+ 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.kkolcz")
+public class AppConfig extends WebMvcConfigurerAdapter{
+       
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
---
+++ obiado/src/main/java/com/kkolcz/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package websystique001.config;
+ 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+ 
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+   
+}
