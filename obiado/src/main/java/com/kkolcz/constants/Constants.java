1a1525e4b1c20c1697973a08363acf92ac770e27
Krzysztof Kołcz <krzysztof.kolcz@gmail.com>
2016-04-06 23:34:37 +0200

    product and product_category database changelog, Constants - start, AdminProductController - start

---
+++ obiado/src/main/java/com/kkolcz/constants/Constants.java
@@ -0,0 +1,23 @@
+package com.kkolcz.constants;
+
+
+public class Constants{
+
+  public static final String A_MODEL_ATTRIBUTE_PRODUCT_LIST = "productList";
+  public static final String A_MODEL_ATTRIBUTE_PRODUCT_COMMAND = "productCommand";
+
+  public static final String A_VIEW_DASHBOARD= "admin/dashboard";
+
+  public static final String A_VIEW_USER_LIST = "admin/userList";
+  public static final String A_VIEW_USER_ADD= "admin/userEdit";
+  public static final String A_VIEW_SUCCESS_USER_ADD= "admin/userEdit";
+
+  public static final String A_VIEW_PRODUCT_LIST = "admin/productList";
+  public static final String A_VIEW_PRODUCT_ADD= "admin/productEdit";
+  public static final String A_VIEW_SUCCESS_PRODUCT_ADD= "admin/productEdit";
+  
+  public static final String U_VIEW_REGISTER = "register";
+  public static final String U_VIEW_SUCCESS_REGISTER = "successRegister";
+
+
+}
---
+++ obiado/src/main/java/com/kkolcz/controller/AdminProductController.java
@@ -0,0 +1,197 @@
+package com.kkolcz.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import javax.validation.Valid;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kkolcz.service.UserService;
+import com.kkolcz.service.UserServiceImpl;
+import com.kkolcz.service.UserProfileService;
+import com.kkolcz.service.UserProfileServiceImpl;
+import com.kkolcz.model.User;
+import com.kkolcz.model.UserProfile;
+import com.kkolcz.command.UserCommand;
+import com.kkolcz.exception.EmailExistsException;
+import com.kkolcz.constants.Constants;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping("/admin")
+@SessionAttributes("roles")
+public class AdminProductController extends BaseController{
+  private final static org.slf4j.Logger logger = LoggerFactory.getLogger(AdminProductController.class);
+
+  @Autowired private ProductService userService;
+  @Autowired private ProductProfileService userProfileService;
+
+  @Autowired private MessageSource messageSource; /* na potrzeby wypisania binding errors */
+
+  @RequestMapping(value = "/product-list", method = RequestMethod.GET)
+  public String productListPage(ModelMap model){
+    List<Product> productList = productService.findAllProducts();
+    model.addAttribute(MODEL_ATTRIBUTE_PRODUCT_LIST,productList);
+    return VIEW_PRODUCT_LIST;
+  }
+
+  @RequestMapping(value = "/add-user", method = RequestMethod.GET)
+  public String addUser(ModelMap model){
+    UserCommand userCommand = new UserCommand();
+    model.addAttribute(MODEL_ATTRIBUTE_USER_COMMAND,userCommand);
+    model.addAttribute(MODEL_ATTRIBUTE_ADMIN,true);
+    return VIEW_USER_ADD;
+  }
+
+
+  /* just like AppController.register */
+  @RequestMapping(value = "/add-user", method = RequestMethod.POST)
+  public ModelAndView register( 
+      @ModelAttribute("userCommand") @Valid UserCommand userCommand, 
+      BindingResult result, 
+      WebRequest request) {
+      User registered = new User();
+      if (!result.hasErrors()) {
+          try {
+              registered = userService.addUser(userCommand);
+          } catch (EmailExistsException e) {
+              result.rejectValue("email", "message.regError");
+          }
+      }
+      if (result.hasErrors()) {
+          return new ModelAndView(VIEW_USER_ADD, MODEL_ATTRIBUTE_USER_COMMAND, userCommand);
+      } 
+      else {
+          return new ModelAndView(VIEW_SUCCESS_USER_ADD,MODEL_ATTRIBUTE_USER_COMMAND , userCommand);
+      }
+  }
+
+  /**
+   * This method will provide UserProfile list to views
+   */
+  @ModelAttribute("roles")
+  public List<UserProfile> initializeProfiles() {
+      return userProfileService.findAll();
+  }
+
+  @RequestMapping(value = "/edit-user-{userId}", method = RequestMethod.GET)
+  public ModelAndView editUser(@PathVariable String userId,ModelMap model) {
+      int id = Integer.parseInt(userId);
+      User user = userService.findById(id);
+      UserCommand userCommand = new UserCommand(user);
+
+      model.addAttribute(MODEL_ATTRIBUTE_USER_COMMAND,userCommand);
+      model.addAttribute(MODEL_ATTRIBUTE_ADMIN,true);
+
+      return new ModelAndView(VIEW_USER_ADD, MODEL_ATTRIBUTE_USER_COMMAND, userCommand);
+  }
+
+  @RequestMapping(value = "/edit-user-{userId}", method = RequestMethod.POST)
+  public ModelAndView editUser(
+      @ModelAttribute("userCommand") @Valid UserCommand userCommand, 
+      BindingResult result, 
+      WebRequest request,
+      @PathVariable String userId) {
+
+      System.out.println("============================");
+      System.out.println("editUser Controller POST");
+      System.out.println(userCommand.getPassword());
+      System.out.println(userCommand.getMatchingPassword());
+      System.out.println("============================");
+
+
+      logger.info("============================");
+      logger.info("editUser Controller POST");
+      logger.info(userCommand.getPassword());
+      logger.info(userCommand.getMatchingPassword());
+      logger.info("============================");
+
+      if(result.hasErrors()){
+        System.out.println("============================");
+        System.out.println("result.hasErrors()");
+        System.out.println("============================");
+        System.out.println("error list:");
+
+        logger.info("============================");
+        logger.info("result.hasErrors()");
+        logger.info("============================");
+        logger.info("error list:");
+        /* if(userService.emailExist(userCommand.getEmail())) { */
+        /*     result.rejectValue("email", "message.regError"); */
+        /* } */
+        /* return VIEW_USER_ADD; */
+        for (Object object : result.getAllErrors()) {
+            /* if(object instanceof FieldError) { */
+            /*     FieldError fieldError = (FieldError) object; */
+            /*     String message = messageSource.getMessage(fieldError, null); */
+            /*     System.out.println(message); */
+            /* } */
+            /* else{ */
+            /*     System.out.println(object); */
+            /* } */
+           if(object instanceof FieldError) {
+                FieldError fieldError = (FieldError) object;
+
+                System.out.println(fieldError.getField());
+                System.out.println(fieldError.getCode());
+                System.out.println(fieldError);
+
+                logger.info(fieldError.getField());
+                logger.info(fieldError.getCode());
+                logger.info(fieldError.toString());
+            }
+
+            if(object instanceof ObjectError) {
+                ObjectError objectError = (ObjectError) object;
+
+                System.out.println(objectError.getCode());
+                logger.info(objectError.getCode());
+            }
+        }
+
+        System.out.println("error list end");
+        System.out.println("============================");
+
+        logger.info("error list end");
+        logger.info("============================");
+        return new ModelAndView(VIEW_USER_ADD, MODEL_ATTRIBUTE_USER_COMMAND, userCommand);
+      }
+
+      try {
+          userCommand.setId(Integer.parseInt(userId));
+          userService.updateUser(userCommand);
+      } catch (EmailExistsException e) {
+          /* nie powinno wystąpić - powyżej sprawdzenie maila */
+          result.rejectValue("email", "message.regError");
+          /* return VIEW_USER_ADD; */
+          return new ModelAndView(VIEW_USER_ADD, MODEL_ATTRIBUTE_USER_COMMAND, userCommand);
+      }
+
+      /* return VIEW_USER_LIST; */
+      return new ModelAndView(VIEW_USER_LIST);
+  }
+}
