09cf0505204cf527b12bac5ef0ec376af43a3b1f
kkolcz <kkolcz@power.com.pl>
2016-06-23 16:48:38 +0200

    jsonExample added

---
+++ jsonExample/src/main/java/com/kkolcz/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.kkolcz.controller;
+
+import com.kkolcz.model.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping(value = "/string", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    public String stringing(@RequestParam(value="name", defaultValue="World") String name) {
+        return String.format(template, name);
+    }
+}
